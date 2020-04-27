35d1a4d26de3cc1fb1c77553350778fb37c5ad18
Lionel <leonjuman@gmail.com>
2020-04-27 13:33:59 +0200

    constructor 1

--- src/agenda/Contacto.java
+++ src/agenda/Contacto.java
@@ -1,9 +1,3 @@
-package agenda;
-
-import java.util.Objects;
-
-/**
- *
  * @author Usuario
  */
 public class Contacto {
@@ -12,19 +6,11 @@ public class Contacto {
     private String mail;
     private int numero;
     
+    //Constructor completo
     Contacto(String nombre,String apellidos,String mail,int numero){
         this.nombre=nombre;
         this.apellidos=apellidos;
         this.mail=mail;
         this.numero=numero;
     }
-    
-
-    Contacto(String mail){
-        this.mail=mail;
-    }
-    
-    Contacto(int numero){
-        this.numero=numero;
-    }
-}
+ }
