e9d4ed9fbaf0f6ddd2dc1c243ac5e013570db8d8
Lionel <leonjuman@gmail.com>
2020-04-27 13:36:35 +0200

    getter and setter

--- src/agenda/Contacto.java
+++ src/agenda/Contacto.java
@@ -20,5 +20,20 @@ public class Contacto {
     Contacto(int numero){
         this.numero=numero;
     }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
  }
      
