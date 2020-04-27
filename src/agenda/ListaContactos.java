28bd2f948fd8e1ec407923a685533b8e2f7eb1dc
Lionel <leonjuman@gmail.com>
2020-04-27 13:48:13 +0200

    busqueda por mail

--- src/agenda/ListaContactos.java
+++ src/agenda/ListaContactos.java
@@ -17,4 +17,17 @@ public class ListaContactos {
         Contacto c=new Contacto(nombre,apellidos,mail,numero); 
         agenda.add(c);                                         
     }
+
+    public boolean buscar(String mail){
+        Contacto aux=new Contacto(mail);
+        if(this.agenda.contains(aux)){ 
+            Contacto imp= this.agenda.get(this.agenda.indexOf(aux));
+            System.out.println(imp); 
+            return true;
+        }else{
+            System.out.println("El contacto introducido no existe");
+            return false;
+        }
+        
+    }
 }
